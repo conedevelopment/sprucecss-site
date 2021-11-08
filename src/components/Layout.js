c9958ee579bc1d4441432ba38b92158479ffbd11
Adam Laki <info@adamlaki.com>
2021-11-08 09:43:32 +0100

    Docs: introduction

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,8 +13,8 @@ function Layout(props) {
     <>
       <Helmet>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="crossorigin" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&amp;display=swap" />
-        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&amp;display=swap" />
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&amp;display=swap" rel="stylesheet" />
       </Helmet>
       <Header />
       {props.children}
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -10,7 +10,7 @@ function Sidebar() {
           <ul>
             <li><Link to="/docs/getting-started/introduction">Introduction</Link></li>
             <li><Link to="/docs/getting-started/download">Download</Link></li>
-            <li><Link to="/docs/getting-started/structure">Structure</Link></li>
+            <li><Link to="/docs/getting-started/structure">Structure and Code</Link></li>
             <li><Link to="/docs/getting-started/sass">Sass</Link></li>
             <li><Link to="/docs/getting-started/accessibility">Accessibility</Link></li>
             <li><Link to="/docs/getting-started/contribute">Contribute</Link></li>
@@ -19,19 +19,19 @@ function Sidebar() {
         </nav>
       </section>
       <section className="sidebar-section">
-        <h3 className="sidebar-section__title">Elements</h3>
-        <nav className="sidebar-section__navigation">
-          <ul>
-            <li><a href="#">Item 1</a></li>
-            <li><a href="#">Item 2</a></li>
-          </ul>
-        </nav>
-      </section>
-      <section className="sidebar-section">
         <h3 className="sidebar-section__title">Customize</h3>
         <nav className="sidebar-section__navigation">
           <ul>
             <li><a href="#">Item 1</a></li>
+            <li><a href="#">Dark Mode</a></li>
+          </ul>
+        </nav>
+      </section>
+      <section className="sidebar-section">
+        <h3 className="sidebar-section__title">Elements</h3>
+        <nav className="sidebar-section__navigation">
+          <ul>
+            <li><Link to="/docs/elements/typography">Typography</Link></li>
             <li><a href="#">Item 2</a></li>
           </ul>
         </nav>
