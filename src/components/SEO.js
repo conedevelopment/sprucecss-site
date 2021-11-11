82c415adbb0bd7df856279f4d4e4d56bc01b84c8
Adam Laki <info@adamlaki.com>
2021-11-11 15:42:00 +0100

    Remove quotes

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -3,8 +3,20 @@ import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
 
 function SEO({ children, location, description, title, image }) {
+  const { site } = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          description
+          twitter
+        }
+      }
+    }
+  `);
+  
   return (
-    <Helmet>
+    <Helmet titleTemplate={`%s - ${site.siteMetadata.title}`}>
       <html lang="en" />
       <title>{title}</title>
       {/* Fav Icons */}
@@ -13,13 +25,14 @@ function SEO({ children, location, description, title, image }) {
       {/* Meta Tags */}
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta charSet="utf-8" />
-      <meta name="description"/>
+      <meta name="description" content={site.siteMetadata.description} />
       {/* Open Graph */}
       {location && <meta property="og:url" content={location.href} />}
       <meta property="og:image" content={image || '/logo.svg'} />
       <meta property="og:title" content={title} key="ogtitle" />
       <meta
         propery="og:site_name"
+        content={site.siteMetadata.title}
         key="ogsitename"
       />
       <meta property="og:description" content={description} key="ogdesc" />
