454d083261b17347bc61c72da09ba8753cbfa7d8
Adam Laki <info@adamlaki.com>
2021-11-11 17:43:08 +0100

    Style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StaticImage } from "gatsby-plugin-image";
 import { Link } from 'gatsby';
 
 // Import components
@@ -6,23 +7,22 @@ import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 
 // Images
-import HeroImage from '../images/hero.svg';
 
 const NotFoundPage = () => {
   return (
     <Layout>
       <SEO title="Error 404" />
       <main id="content" className="hero">
-        <div className="hero__container">
-          <div className="hero__inner">
-            <div className="hero__caption">
-              <h1 className="hero__title">Error 404</h1>
-              <p className="hero__description">We couldn’t find what you were looking for.</p>
-              <div className="hero__btns">
-                <Link to="/" className="btn btn--primary btn--lg">Go home</Link>
+        <div className="error-404__container">
+          <div className="error-404__inner">
+            <StaticImage src="../images/404.svg" alt="Page not found illustration..." className="error-404__image" />
+            <div className="error-404__caption">
+              <h1 className="error-404__title">Ooops! You weren’t supposed to see this</h1>
+              <p className="error-404__description">We couldn’t find the page you were looking for.</p>
+              <div className="error-404__btns">
+                <Link to="/" className="btn btn--primary">Go home</Link>
               </div>
             </div>
-            <HeroImage className="hero__image" />
           </div>
         </div>
       </main>
