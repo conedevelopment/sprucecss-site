2290d9b15805915e78d7dc3c45342379cfdc64dd
Adam Laki <info@adamlaki.com>
2021-11-11 14:59:43 +0100

    Switch to yarn

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -3,20 +3,8 @@ import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
 
 function SEO({ children, location, description, title, image }) {
-  const { site } = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          title
-          description
-          twitter
-        }
-      }
-    }
-  `);
-  
   return (
-    <Helmet titleTemplate={`%s - ${site.siteMetadata.title}`}>
+    <Helmet>
       <html lang="en" />
       <title>{title}</title>
       {/* Fav Icons */}
@@ -25,14 +13,13 @@ function SEO({ children, location, description, title, image }) {
       {/* Meta Tags */}
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta charSet="utf-8" />
-      <meta name="description" content={site.siteMetadata.description} />
+      <meta name="description"/>
       {/* Open Graph */}
       {location && <meta property="og:url" content={location.href} />}
       <meta property="og:image" content={image || '/logo.svg'} />
       <meta property="og:title" content={title} key="ogtitle" />
       <meta
         propery="og:site_name"
-        content={site.siteMetadata.title}
         key="ogsitename"
       />
       <meta property="og:description" content={description} key="ogdesc" />
