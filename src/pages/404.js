b7d5d8a3240f1b175949f8ee3b6b9d07266036e3
Adam Laki <info@adamlaki.com>
2021-11-12 19:46:42 +0100

    Cleaning

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StaticImage } from "gatsby-plugin-image";
 import { Link } from 'gatsby';
 
 // Import components
@@ -7,15 +6,16 @@ import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 
 // Images
+import NotFoundImage from '../images/not-found.svg';
 
-const NotFoundPage = () => {
+function NotFoundPage() {
   return (
     <Layout>
       <SEO title="Error 404" />
       <main id="content" className="hero">
         <div className="error-404__container">
           <div className="error-404__inner">
-            <StaticImage src="../images/404.svg" alt="Page not found illustration..." className="error-404__image" />
+            <NotFoundImage alt="Page not found illustration..." className="error-404__image" />
             <div className="error-404__caption">
               <h1 className="error-404__title">Ooops! You weren’t supposed to see this</h1>
               <p className="error-404__description">We couldn’t find the page you were looking for.</p>
