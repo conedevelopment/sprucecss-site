5b1a811368bea5428a12157c5276ce83d747f823
Adam Laki <info@adamlaki.com>
2021-11-15 20:13:12 +0100

    WIP

--- src/components/DocsCards.js
+++ src/components/DocsCards.js
@@ -8,15 +8,15 @@ function DocsCards(props) {
         <div className="docs-cards__inner">
           <Link to="/docs/getting-started/introduction" className="docs-card">
             <span className="docs-card__serial-number">01</span>
-            <h2 className="docs-card__title">Introduction</h2>
+            <h2 className="docs-card__title">Intro&shy;duction</h2>
           </Link>
           <Link to="/docs/getting-started/installation" className="docs-card">
             <span className="docs-card__serial-number">02</span>
-            <h2 className="docs-card__title">Installation</h2>
+            <h2 className="docs-card__title">Instal&shy;lation</h2>
           </Link>
           <Link to="/docs/getting-started/structure-and-code" className="docs-card">
             <span className="docs-card__serial-number">03</span>
-            <h2 className="docs-card__title">Structure and Code</h2>
+            <h2 className="docs-card__title">Struc&shy;ture and Code</h2>
           </Link>
           <Link to="/docs/getting-started/sass" className="docs-card">
             <span className="docs-card__serial-number">04</span>
@@ -24,11 +24,11 @@ function DocsCards(props) {
           </Link>
           <Link to="/docs/getting-started/accessibility" className="docs-card">
             <span className="docs-card__serial-number">05</span>
-            <h2 className="docs-card__title">Accessibility</h2>
+            <h2 className="docs-card__title">Access&shy;ibility</h2>
           </Link>
           <Link to="/docs/getting-started/contribute" className="docs-card">
             <span className="docs-card__serial-number">06</span>
-            <h2 className="docs-card__title">Contribute</h2>
+            <h2 className="docs-card__title">Contri&shy;bute</h2>
           </Link>
         </div>
       </div>
