d311ce10392dfef63a5469c0b9cccf796dcb6d1c
Adam Laki <info@adamlaki.com>
2021-11-20 12:47:31 +0100

    Style footer

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Highlight, { defaultProps } from 'prism-react-renderer';
-import theme from 'prism-react-renderer/themes/nightOwlLight';
+import theme from 'prism-react-renderer/themes/nightOwl';
 
 function CodeBlock(props) {
   const className = props?.children?.props?.className || '';
