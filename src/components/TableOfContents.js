a108ff3d8b6fc96b1f59a6b2721d75dfc38f25f3
Adam Laki <info@adamlaki.com>
2021-12-28 16:46:01 +0100

    Fix TOC warning

--- src/components/TableOfContents.js
+++ src/components/TableOfContents.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
-export default ({ headings }) => (
-  <ul>
-    {headings
-      .filter(heading => heading.depth !== 1)
-      .map(heading => (
-        <li key={heading.title}>
-          <a href={heading.url}>{heading.title}</a>
-        </li>
-      ))}
-  </ul>
-)
+ function TableOfContent({ headings }) {
+  return (
+    <ul>
+      {headings
+        .filter(heading => heading.depth !== 1)
+        .map(heading => (
+          <li key={heading.title}>
+            <a href={heading.url}>{heading.title}</a>
+          </li>
+        ))}
+    </ul>
+  )
+}
+
+export default TableOfContent;
