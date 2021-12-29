14a49d342ef92d1aad895f26f9d0d60039cec5a3
Adam Laki <info@adamlaki.com>
2021-12-29 15:24:11 +0100

    Preview updates

--- src/components/mdx/Table.js
+++ src/components/mdx/Table.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function table(props) {
-  console.log(props);
   return (
     <div className="table-responsive">
       <table className={`table ${props.className ? props.className : ''}`}>{props.children}</table>
