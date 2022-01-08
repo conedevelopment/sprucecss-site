95d8e260cd83c3146ea7d10072448a34990a464a
Adam Laki <info@adamlaki.com>
2022-01-08 20:35:39 +0100

    Fix favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 module.exports = {
   siteMetadata: {
     description: 'Spruce is a minimalistic CSS starter Framework on Sass.',
-    icon: 'src/images/icon.svg',
     github: 'conedevelopment',
     version: '0.1.3',
     siteUrl: 'https://sprucecss.com',
