3126e2b5c34c8a98faddae714fd8d87c9dce3c8c
Adam Laki <info@adamlaki.com>
2022-01-09 16:02:23 +0100

    Update notifications

--- src/components/mdx/Notification.js
+++ src/components/mdx/Notification.js
@@ -5,20 +5,24 @@ import CheckIcon from "../../images/icons/check.svg";
 import InfoIcon from "../../images/icons/info.svg";
 import CrossIcon from "../../images/icons/cross.svg";
 
-function Notification({children, type}) {
-  let icon;
+function Notification({children, type, icon = true}) {
+  let iconSVG;
 
-  if (type === 'wrong') {
-    icon = <CrossIcon className="notification__icon" />;
-  } else if (type === 'correct') {
-    icon = <CheckIcon className="notification__icon" />
-  } else {
-    icon = <InfoIcon className="notification__icon" />;
+  switch(type) {
+    case 'wrong':
+      iconSVG = <CrossIcon className="notification__icon" />;
+      break;
+    case 'correct':
+      iconSVG = <CheckIcon className="notification__icon" />;
+      break;
+    case 'info':
+      iconSVG = <InfoIcon className="notification__icon" />;
+      break;
   }
 
   return (
     <div className={`notification notification--${type ?? 'info'}`}>
-      {icon}
+      {icon ? iconSVG : null}
       <div className="notification__content">
         {children}
       </div>
