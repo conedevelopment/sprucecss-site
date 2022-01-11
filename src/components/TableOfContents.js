267419c5440be8d4601992ade7d01d683f6ddf9a
Adam Laki <info@adamlaki.com>
2022-01-11 09:31:29 +0100

    Cleanup

--- src/components/TableOfContents.js
+++ src/components/TableOfContents.js
@@ -1,68 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React from 'react';
 
-function getIds(items) {
-  return items.reduce((acc, item) => {
-    if (item.url) {
-      // url has a # as first character, remove it to get the raw CSS-id
-      acc.push(item.url.slice(1));
-    }
-    if (item.items) {
-      acc.push(...getIds(item.items));
-    }
-    return acc;
-  }, []);
-}
-
-function useActiveId(itemIds) {
-  const [activeId, setActiveId] = useState('test');
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            setActiveId(entry.target.id);
-          }
-        });
-      },
-      { rootMargin: '0% 0% -80% 0%' }
-    );
-    itemIds.forEach((id) => {
-      observer.observe(document.getElementById(id));
-    });
-    return () => {
-      itemIds.forEach((id) => {
-        observer.unobserve(document.getElementById(id));
-      });
-    };
-  }, [itemIds]);
-  return activeId;
-}
-
-function renderItems(items, activeId) {
+ function TableOfContents({ headings }) {
   return (
     <ul>
-      {items.map((item) => {
-        return (
-          <li key={item.url}>
-            <a
-              href={item.url}
-              className={activeId === item.url.slice(1) ? 'is-active' : ''}
-            >
-              {item.title}
-            </a>
-            {item.items && renderItems(item.items, activeId)}
+      {headings
+        .filter(heading => heading.depth !== 1)
+        .map(heading => (
+          <li key={heading.title}>
+            <a href={heading.url}>{heading.title}</a>
           </li>
-        );
-      })}
+        ))}
     </ul>
-  );
-}
-
-function TableOfContents({ headings }) {
-  const idList = getIds(headings);
-  const activeId = useActiveId(idList);
-  return renderItems(headings, activeId);
+  )
 }
 
 export default TableOfContents;
