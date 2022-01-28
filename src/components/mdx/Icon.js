c7d2cb0bd47e53bd459359f6ad9cc98e14c47c79
Adam Laki <info@adamlaki.com>
2022-01-28 19:49:48 +0100

    Add icon button section

--- src/components/mdx/Icon.js
+++ src/components/mdx/Icon.js
@@ -4,6 +4,7 @@ import React from 'react';
 import CheckIcon from "../../images/icons/check.svg";
 import InfoIcon from "../../images/icons/info.svg";
 import CrossIcon from "../../images/icons/cross.svg";
+import Copy from "../../images/icons/copy.svg";
 import ArrowRightIcon from "../../images/icons/arrow-right.svg";
 
 function Icon({type, className}) {
@@ -12,9 +13,11 @@ function Icon({type, className}) {
   if (type === 'check') {
     icon = <CheckIcon className={className} />;
   } else if (type === 'cross') {
-    icon = <CrossIcon className={className} />
+    icon = <CrossIcon className={className} />;
   } else if (type === 'info') {
     icon = <InfoIcon className={className} />;
+  } else if (type === 'copy') {
+    icon = <Copy className={className} />;
   } else if (type === 'arrow-right') {
     icon = <ArrowRightIcon className={className} />;
   }
