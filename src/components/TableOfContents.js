0138b598019adb067e2829b706215397d1f144cc
Adam Laki <info@adamlaki.com>
2022-02-13 17:47:44 +0100

    Exclude Example(s) and Argument(s) from TOC

--- src/components/TableOfContents.js
+++ src/components/TableOfContents.js
@@ -5,7 +5,7 @@ function TableOfContents({ headings }) {
   return (
     <ul>
       {headings
-        .filter(heading => heading.depth > 1 && heading.depth < 4)
+        .filter(heading => heading.depth > 1 && heading.depth < 4 && heading.value !== 'Example(s)' && heading.value !== 'Argument(s)')
         .map(heading => (
           <li className={`heading-${heading.depth}`} key={heading.value}>
             <a href={`#${getAnchor(heading.value)}`}>{heading.value}</a>
