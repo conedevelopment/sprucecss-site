4753072f08cfd8593404a7b5e216bc9654f60be9
Adam Laki <info@adamlaki.com>
2022-04-05 18:02:00 +0200

    Update Spruce version

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     description: 'Spruce CSS is a modern, minimal CSS Framework built on Sass.',
     github: 'conedevelopment',
-    version: '0.5.0',
+    version: '0.6.0',
     siteUrl: 'https://sprucecss.com',
     slogan: [
       '🎉 Another CSS Framework',
