ee0443e8be7fc4d85ca227a6a303220dc9701a74
Adam Laki <info@adamlaki.com>
2022-04-18 18:46:20 +0200

    Add Google Font preconnect

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -12,7 +12,8 @@ export default function Layout(props) {
   return (
     <>
       <Helmet>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="crossorigin" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&amp;display=swap" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;700&amp;display=swap" />
       </Helmet>
