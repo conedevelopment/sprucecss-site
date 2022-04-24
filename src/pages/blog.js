b6fa6e7a6ca9815437ad7818eed35b689d271e24
Adam Laki <info@adamlaki.com>
2022-04-24 17:25:19 +0200

    Blog WIP

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -12,13 +12,22 @@ export default function Blog() {
       <Seo title="Blog" />
       <main id="content" className="layout-blog">
         <div className="layout-blog__container">
-          <PageHeading
-            title="Blog"
-            description="Quick and straightforward examples to see how you can start with and use Spruce CSS."
-          >
-            <a href="https://github.com/conedevelopment/sprucecss-examples/archive/refs/heads/master.zip">Download</a>
-            <a href="https://github.com/conedevelopment/sprucecss-examples">Github</a>
-          </PageHeading>
+          <div className="layout-blog__inner">
+            <div className="layout-blog__heading">
+              <PageHeading
+                title="Blog"
+                description="Quick and straightforward examples to see how you can start with and use Spruce CSS."
+              />
+            </div>
+            <div className="layout-blog__list">
+              <div className="blog-list">
+                <div className="blog-item">
+                  <h2 className="blog-item__title"><a href="#" className="blog-item__link">Blog article #1</a></h2>
+                  <p className="blog-item__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus viverra lectus, sit amet dictum ipsum lacinia accumsan. Donec dapibus condimentum varius.</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </Layout>
