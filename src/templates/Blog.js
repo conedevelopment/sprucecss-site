8031f534db1125d22b80ad581057add81e35deb2
Adam Laki <info@adamlaki.com>
2022-04-30 14:26:52 +0200

    Fixes

--- src/templates/Blog.js
+++ src/templates/Blog.js
@@ -15,12 +15,12 @@ export default function Post({ data: { mdx: post } }) {
   return (
     <Layout>
       <Seo title={title} />
-      <main id="content" class="post">
-        <div class="post-heading">
-          <div class="container--narrow">
-            <div class="post-heading__inner">
-              <h1 class="post-heading__title">{title}</h1>
-              <ul class="breadcrumb-list">
+      <main id="content" className="post">
+        <div className="post-heading">
+          <div className="container--narrow">
+            <div className="post-heading__inner">
+              <h1 className="post-heading__title">{title}</h1>
+              <ul className="breadcrumb-list">
                 <li>
                   <Link to="/">Home</Link>
                 </li>
@@ -31,34 +31,34 @@ export default function Post({ data: { mdx: post } }) {
                   <span aria-current="page">{title}</span>
                 </li>
               </ul>
-              <div class="post-heading__meta">
-                <a class="post-author" href="https://twitter.com/_iamadam" rel="noopener noreferrer" target="_blank">
-                  <img class="post-author__avatar" src="https://secure.gravatar.com/avatar/fd9479a898c593601efd2fe758a86dba?s=96&amp;d=mm&amp;r=g" alt="" aria-hidden="true" width="38" height="38" />
-                  <span class="post-author__name">by Adam Laki</span>
+              <div className="post-heading__meta">
+                <a className="post-author" href="https://twitter.com/_iamadam" rel="noopener noreferrer" target="_blank">
+                  <img className="post-author__avatar" src="https://secure.gravatar.com/avatar/fd9479a898c593601efd2fe758a86dba?s=96&amp;d=mm&amp;r=g" alt="" aria-hidden="true" width="38" height="38" />
+                  <span className="post-author__name">by Adam Laki</span>
                 </a>
                 <span>
                   Posted in {post.frontmatter.tags
                     .filter(tag => tag.length > 0)
                     .map((tag, i) => (
-                      <>
-                        <Link to={`/blog/tag/${tag}`}>
+                      <span key={tag}>
+                        <Link to={`/blog/tag/${tag}`} className="blog-card__tag">
                           #{tag}
                         </Link>
                         {i < post.frontmatter.tags.length - 1 ? ', ' : ''}
-                      </>
+                      </span>
                     ))
                   }
                 </span>
                 <span>
-                  <span class="sr-only">Posted on</span>
+                  <span className="sr-only">Posted on</span>
                   <span>{post.frontmatter.date}</span>
                 </span>
               </div>
             </div>
           </div>
         </div>
-        <div class="container--wide">
-          <div class="post-content">
+        <div className="container--wide">
+          <div className="post-content">
             <GatsbyImage image={image} alt={post.frontmatter.alt} />
             <MDXRenderer>{body}</MDXRenderer>
           </div>
