1ebda33d5dab88d84e1f37dc362dde794580c548
Adam Laki <info@adamlaki.com>
2022-05-05 20:01:34 +0200

    Remove copy button from code-block

--- src/components/mdx/CodeBlock.js
+++ src/components/mdx/CodeBlock.js
@@ -6,17 +6,12 @@ import nightOwlLight from 'prism-react-renderer/themes/nightOwlLight';
 // Components
 import ThemeContext from '../../components/ThemeContext';
 
-// Images
-import CopyIcon from '../../images/icons/copy.svg';
-import CopiedIcon from '../../images/icons/copied.svg';
-
 export default function CodeBlock(props) {
   let [theme, setTheme] = useContext(ThemeContext);
   let systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 
   const className = props?.children?.props?.className || '';
   const matches = className.match(/language-(?<lang>.*)/);
-  const [isCopied, setIsCopied] = React.useState(false);
 
   const themes = {
     light: nightOwlLight,
@@ -34,17 +29,6 @@ export default function CodeBlock(props) {
       theme={themes[(theme === 'system' ? systemTheme : theme)]}>
       {({className, style, tokens, getLineProps, getTokenProps}) => (
         <div className="prism-code-wrapper">
-          <button
-            className="btn btn--primary btn--sm prism-code__copy-btn"
-            onClick={() => {
-              navigator.clipboard.writeText(props?.children?.props?.children.trim())
-              setIsCopied(true)
-              setTimeout(() => setIsCopied(false), 3000)
-            }}
-            aria-label={isCopied ? 'Copied' : 'Copy'}
-          >
-            {isCopied ? <CopiedIcon /> : <CopyIcon />}
-          </button>
           <pre className={className} style={{...style}}>
             <span className="prism-code__language">{matches?.groups?.lang || ''}</span>
             <code>
