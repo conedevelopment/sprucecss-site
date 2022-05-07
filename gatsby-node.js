38f8d8954bcb594d67fbdde50513a81027591711
Adam Laki <info@adamlaki.com>
2022-05-07 11:42:18 +0200

    Test

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,7 +15,6 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 
 exports.createPages = async ({ graphql, actions }) => {
   const docTemplate = path.resolve('./src/templates/docs.js');
-  const blogTemplate = path.resolve('./src/templates/blog.js');
   const tagTemplate = path.resolve('./src/templates/tag.js');
 
   const dataDocs = await graphql(`
@@ -34,33 +33,7 @@ exports.createPages = async ({ graphql, actions }) => {
     }
   `);
 
-  const dataBlog = await graphql(`
-    query {
-      posts: allMdx(
-        sort: {order: DESC, fields: frontmatter___date}
-        filter: {fields: {collection: {eq: "blog"}}}
-      ) {
-        nodes {
-          slug
-          frontmatter {
-            title
-            date
-            tags
-          }
-        }
-      }
-      tagsGroup: allMdx(limit: 2000) {
-        group(field: frontmatter___tags) {
-          fieldValue
-        }
-      }
-    }
-  `);
-
-
   const docPages = dataDocs.data.docs.nodes;
-  const blogPages = dataBlog.data.posts.nodes;
-  const tags = dataBlog.data.tagsGroup.group;
 
   docPages.forEach((doc, index) => {
     actions.createPage({
@@ -71,27 +44,6 @@ exports.createPages = async ({ graphql, actions }) => {
         prev: index === 0 ? null : docPages[index - 1],
         next: index === (docPages.length - 1) ? null : docPages[index + 1]
       }
-    });
-  });
-
-  blogPages.forEach((post) => {
-    actions.createPage({
-      path: `blog/${post.slug}`,
-      component: blogTemplate,
-      context: {
-        slug: post.slug,
-      }
-    });
-  });
-
-  // Make tag pages
-  tags.forEach(tag => {
-    actions.createPage({
-      path: `blog/tag/${tag.fieldValue}`,
-      component: tagTemplate,
-      context: {
-        tag: tag.fieldValue,
-      },
     });
   });
 }
