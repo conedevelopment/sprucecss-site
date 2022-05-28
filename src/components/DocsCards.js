cfc24ead06305fbeb65549e9586613cb80cee487
Adam Laki <info@adamlaki.com>
2022-05-28 11:14:06 +0200

    WIP

--- src/components/DocsCards.js
+++ src/components/DocsCards.js
@@ -11,27 +11,27 @@ export default function DocsCards(props) {
         <div className="docs-cards__inner">
           <Link to="/docs/getting-started/introduction" className="docs-card">
             <span className="docs-card__serial-number">01</span>
-            <h2 className="docs-card__title">Intro&shy;duction</h2>
+            <h2 className="docs-card__title h3">Intro&shy;duction</h2>
           </Link>
           <Link to="/docs/getting-started/installation" className="docs-card">
             <span className="docs-card__serial-number">02</span>
-            <h2 className="docs-card__title">Instal&shy;lation</h2>
+            <h2 className="docs-card__title h3">Instal&shy;lation</h2>
           </Link>
           <Link to="/docs/getting-started/structure-and-code" className="docs-card">
             <span className="docs-card__serial-number">03</span>
-            <h2 className="docs-card__title">Struc&shy;ture and Code</h2>
+            <h2 className="docs-card__title h3">Struc&shy;ture and Code</h2>
           </Link>
           <Link to="/docs/elements/typography" className="docs-card">
             <span className="docs-card__serial-number">04</span>
-            <h2 className="docs-card__title">Typography</h2>
+            <h2 className="docs-card__title h3">Typography</h2>
           </Link>
           <Link to="/docs/elements/tables" className="docs-card">
             <span className="docs-card__serial-number">05</span>
-            <h2 className="docs-card__title">Tables</h2>
+            <h2 className="docs-card__title h3">Tables</h2>
           </Link>
           <Link to="/docs/elements/buttons" className="docs-card">
             <span className="docs-card__serial-number">06</span>
-            <h2 className="docs-card__title">Buttons</h2>
+            <h2 className="docs-card__title h3">Buttons</h2>
           </Link>
         </div>
       </div>
