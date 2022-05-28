151c6f74f53441e45cd27c8d0632dd38980d7914
Adam Laki <info@adamlaki.com>
2022-05-28 12:10:19 +0200

    WIP

--- src/components/GettingStarted.js
+++ src/components/GettingStarted.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
+// Images
+import Icon from '../images/icons/examples.svg';
+
 export default function GettingStarted({ title, description, url, github, children }) {
   return (
     <div className="getting-started">
       <div className="container">
+        <div className="heading">
+          <h2 className="heading__title">Explore</h2>
+        </div>
         <div className="getting-started__inner">
-          <div className="heading">
-            <h2 className="heading__title">Explore</h2>
+          <div>
+            <h3>Documentation</h3>
+            <p>Nam eget accumsan leo, ac laoreet nibh. Vivamus et justo lobortis, egestas turpis et, convallis orci.</p>
+          </div>
+          <div>
+            <Icon style={{width: "4rem", height: "4rem"}} />
+            <h3>Examples</h3>
+            <p>Praesent in enim interdum, eleifend nunc sed, mollis enim. Sed lobortis neque ex, nec condimentum felis porta eget.</p>
+          </div>
+          <div>
+            <h3>Blog</h3>
+            <p>Id egestas purus lorem sed leo. Nunc lectus leo, molestie non tortor sit amet, tincidunt dictum dui.</p>
           </div>
         </div>
       </div>
