c59ad0a8c085c9cfb5b8731510c1be1f0ae9da24
Adam Laki <info@adamlaki.com>
2022-05-29 16:04:19 +0200

    Updates

--- src/components/DocsCards.js
+++ src/components/DocsCards.js
@@ -9,29 +9,29 @@ export default function DocsCards(props) {
           <h2 className="heading__title">Documentation</h2>
         </div>
         <div className="docs-cards__inner">
-          <Link to="/docs/getting-started/introduction" className="docs-card">
-            <span className="docs-card__serial-number">01</span>
-            <h2 className="docs-card__title h3">Intro&shy;duction</h2>
-          </Link>
           <Link to="/docs/getting-started/installation" className="docs-card">
-            <span className="docs-card__serial-number">02</span>
+            <span className="docs-card__serial-number">01</span>
             <h2 className="docs-card__title h3">Instal&shy;lation</h2>
           </Link>
-          <Link to="/docs/getting-started/structure-and-code" className="docs-card">
+          <Link to="/docs/customization/themes" className="docs-card">
+            <span className="docs-card__serial-number">02</span>
+            <h2 className="docs-card__title h3">Themes</h2>
+          </Link>
+          <Link to="/docs/sass/mixins" className="docs-card">
             <span className="docs-card__serial-number">03</span>
-            <h2 className="docs-card__title h3">Struc&shy;ture and Code</h2>
+            <h2 className="docs-card__title h3">Mixins</h2>
           </Link>
           <Link to="/docs/elements/typography" className="docs-card">
             <span className="docs-card__serial-number">04</span>
             <h2 className="docs-card__title h3">Typography</h2>
           </Link>
-          <Link to="/docs/elements/tables" className="docs-card">
-            <span className="docs-card__serial-number">05</span>
-            <h2 className="docs-card__title h3">Tables</h2>
-          </Link>
           <Link to="/docs/elements/buttons" className="docs-card">
-            <span className="docs-card__serial-number">06</span>
+            <span className="docs-card__serial-number">05</span>
             <h2 className="docs-card__title h3">Buttons</h2>
+          </Link>
+          <Link to="/docs/elements/forms" className="docs-card">
+            <span className="docs-card__serial-number">06</span>
+            <h2 className="docs-card__title h3">Forms</h2>
           </Link>
         </div>
       </div>
