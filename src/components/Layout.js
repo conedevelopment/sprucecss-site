9925743a80ad4ae05034ed8c826702547ff691ef
Adam Laki <info@adamlaki.com>
2022-05-30 08:08:14 +0200

    Font experiment

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -17,6 +17,7 @@ export default function Layout(props) {
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;700&display=swap" />
         <link href="https://fonts.googleapis.com/css2?family=Sora:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
       </Helmet>
       <Header />
       {props.children}
