4817f80f81f65b4229e15b2e284e606c7392c2a4
Adam Laki <info@adamlaki.com>
2022-06-04 17:24:44 +0200

    eslint... 😢

--- src/templates/Blog.js
+++ src/templates/Blog.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useStaticQuery, graphql  } from 'gatsby';
+import { Link, graphql  } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
