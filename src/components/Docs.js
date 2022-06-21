a2845a6f2e0e4b44522060b8bf4886ee11f3e808
Adam Laki <info@adamlaki.com>
2022-06-21 15:58:51 +0200

    Refactor, cleanup

--- src/components/Docs.js
+++ src/components/Docs.js
@@ -9,30 +9,54 @@ export default function Docs() {
           <h2 className="heading__title">Documentation</h2>
         </div>
         <div className="doc-cards__inner">
-          <Link to="/docs/getting-started/installation" className="doc-card">
+          <div className="doc-card">
             <span className="doc-card__serial-number">01</span>
-            <h3 className="doc-card__title h3">Instal&shy;lation</h3>
-          </Link>
-          <Link to="/docs/customization/themes" className="doc-card">
+            <h3 className="doc-card__title">
+                <Link to="/docs/getting-started/installation" className="doc-card__link">
+                  Instal&shy;lation
+                </Link>
+              </h3>
+          </div>
+          <div className="doc-card">
             <span className="doc-card__serial-number">02</span>
-            <h3 className="doc-card__title h3">Themes</h3>
-          </Link>
-          <Link to="/docs/sass/mixins" className="doc-card">
+            <h3 className="doc-card__title">
+              <Link to="/docs/customization/themes" className="doc-card__link">
+                Themes
+              </Link>
+            </h3>
+          </div>
+          <div className="doc-card">
             <span className="doc-card__serial-number">03</span>
-            <h3 className="doc-card__title h3">Mixins</h3>
-          </Link>
-          <Link to="/docs/elements/typography" className="doc-card">
+            <h3 className="doc-card__title">
+              <Link to="/docs/sass/mixins" className="doc-card__link">
+                Mixins
+              </Link>
+            </h3>
+          </div>
+          <div className="doc-card">
             <span className="doc-card__serial-number">04</span>
-            <h3 className="doc-card__title h3">Typography</h3>
-          </Link>
-          <Link to="/docs/elements/buttons" className="doc-card">
+            <h3 className="doc-card__title">
+              <Link to="/docs/elements/typography" className="doc-card__link">
+                Typography
+              </Link>
+            </h3>
+          </div>
+          <div className="doc-card">
             <span className="doc-card__serial-number">05</span>
-            <h3 className="doc-card__title h3">Buttons</h3>
-          </Link>
-          <Link to="/docs/elements/forms" className="doc-card">
+            <h3 className="doc-card__title">
+              <Link to="/docs/elements/buttons" className="doc-card__link">
+                Buttons
+              </Link>
+            </h3>
+          </div>
+          <div className="doc-card">
             <span className="doc-card__serial-number">06</span>
-            <h3 className="doc-card__title h3">Forms</h3>
-          </Link>
+            <h3 className="doc-card__title">
+              <Link to="/docs/elements/forms" className="doc-card__link">
+                Forms
+              </Link>
+            </h3>
+          </div>
         </div>
       </div>
     </div>
