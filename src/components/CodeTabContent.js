cf784a7bd704c9eda1124de73e352b685b39e284
Adam Laki <info@adamlaki.com>
2022-11-24 21:23:33 +0100

    Add copy button

--- src/components/CodeTabContent.js
+++ src/components/CodeTabContent.js
@@ -5,7 +5,7 @@ import CodeBlockCopyBtn from './CodeBlockCopyBtn';
 export default function CodeTabContent({id, activeTab, code, children}) {
   return (
     activeTab === id ? <div id={`tab-content-${id}`} role="tabpanel" aria-labelledby={`tab-control-${id}`}>
-      {id !== 'preview' ? <div><CodeBlockCopyBtn/><CodeBlock className={id}>{code.trim()}</CodeBlock></div> : null}
+      {id !== 'preview' ? <div className="code-tab__inner"><CodeBlockCopyBtn code={code.trim()}/><CodeBlock className={id}>{code.trim()}</CodeBlock></div> : null}
       {id === 'preview' ? children : null}
     </div>
     : null
