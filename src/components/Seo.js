141c9110731730e1ced01b13dd1023aaae389226
Adam Laki <info@adamlaki.com>
2023-01-04 18:22:41 +0100

    Switch to local google fonts

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -8,10 +8,6 @@ export default function Seo({ children, location, description, title, image, fro
     <>
       <title>{frontPage ? `${title}` : `${title} - ${siteMetadata.title}`}</title>
 
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;700&family=Sora:wght@400;500;600;700&display=swap" />
-
       {/* Meta Tags */}
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta charSet="utf-8" />
