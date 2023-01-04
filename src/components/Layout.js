7a64e9ec50528f486b944f7698228b32edf67657
Adam Laki <info@adamlaki.com>
2023-01-04 15:47:58 +0100

    Add Google Fonts back

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -38,15 +38,3 @@ export default function Layout(props) {
     </>
   );
 }
-
-/*
-export function Head() {
-  return (
-    <>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;700&family=Sora:wght@400;500;600;700&display=swap" />
-    </>
-  )
-}
-*/
