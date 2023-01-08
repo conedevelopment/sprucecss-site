9f5a2ae2f731d952059f60832f09bc4282d42fc9
Viktor Szépe <viktor@szepe.net>
2023-01-08 14:26:26 +0000

    Add eslint standards and plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,28 @@
 module.exports = {
   globals: {
-    __PATH_PREFIX__: true,
+    __PATH_PREFIX__: true
   },
-  extends: `react-app`,
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'react-app',
+    'plugin:react/recommended',
+    'standard'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'react/prop-types': 0,
+    'semi': ['error', 'always'],
+    'space-before-function-paren': ['error', 'never']
+  }
 }
