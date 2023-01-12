9a34e578034720b1b16d81b96db113376c54407a
Adam Laki <info@adamlaki.com>
2023-01-12 15:25:18 +0100

    Modify eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,31 @@
 module.exports = {
   globals: {
-    __PATH_PREFIX__: true,
+    __PATH_PREFIX__: true
   },
-  extends: `react-app`,
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'react-app',
+    'plugin:react/recommended',
+    'standard'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'react/prop-types': 0, // @TODO This rule is muted.
+    semi: ['error', 'always'],
+    'space-before-function-paren': ['error', {
+      anonymous: 'always',
+      named: 'never'
+    }]
+  }
 }
