daf6a91b04324728788a9723327688c5011ff27d
Adam Laki <info@adamlaki.com>
2023-04-08 14:52:05 +0200

    Hero WIP

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import { Link } from 'gatsby';
-
-// Images
-import CopyIcon from '../images/icons/copy.svg';
+import { StaticImage } from 'gatsby-plugin-image';
 
 export default function Hero() {
-  async function handleCopy() {
-    await navigator.clipboard.writeText('npm install sprucecss');
-    alert('Install command copied to the clipboard!');
-  }
-
   return (
     <div className="hero">
-      <div className="container">
-        <h1 className="hero__title">Spruce CSS <span>Framework</span></h1>
-        <p className="hero__description">A low-level, lightweight and modern CSS design system, authoring tool built on Sass. Give your project a solid foundation.</p>
-        <div className="hero__btns">
-          <Link to="/docs/getting-started/introduction" className="btn btn--primary btn--lg btn--rounded">Get Started</Link>
-          <button className="btn btn--install btn--icon btn--lg btn--rounded" onClick={handleCopy}>
-            <CopyIcon className="btn__icon" />
-            npm install sprucecss
-          </button>
+      <div className="container container--wide">
+        <div className="hero__inner">
+          <div className="hero__caption">
+            <h1 className="hero__title">A minimalistic, low-level CSS framework</h1>
+            <p className="hero__description">A low-level, lightweight and modern CSS design system, authoring tool built on Sass. Give your project a solid foundation.</p>
+            <div className="hero__btns">
+              <Link to="/docs/getting-started/introduction" className="btn btn--primary btn--lg btn--rounded">Get Started</Link>
+              <Link to="/docs/getting-started/introduction" className="btn btn--primary-outlined btn--lg btn--rounded">Components</Link>
+            </div>
+          </div>
+          <StaticImage
+            src="../images/torus.png"
+            alt=""
+            placeholder="blurred"
+          />
         </div>
       </div>
     </div>
