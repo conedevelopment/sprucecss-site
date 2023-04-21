66ee596c606502bc9504bbbbccc19539a3c8adc7
Adam Laki <info@adamlaki.com>
2023-04-21 10:09:35 +0200

    Update FAQ links

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,3 +1,4 @@
+import { Link } from 'gatsby';
 import AccordionCard from './card/Accordion';
 import AccordionList from './AccordionList';
 import Heading from './Heading';
@@ -15,7 +16,7 @@ export default function Faq() {
           />
           <AccordionList>
             <AccordionCard title="Who is Spruce For?">
-              <p>If you like to handle your CSS/SCSS in separate files and write your code, this tool is for you. It uses just a few helper classes; most [components](/ui/getting-started/introduction/) are drop-ins.</p>
+              <p>If you like to handle your CSS/SCSS in separate files and write your code, this tool is for you. It uses just a few helper classes; most <Link to="/ui/getting-started/introduction/">components</Link> are drop-ins.</p>
             </AccordionCard>
             <AccordionCard title="It is Modern and Lightweight">
               <p>By modern, we mean it uses new CSS stuff sometimes at the expense of backward compatibility. Fortunately, nowadays, the new CSS features are released in bulk across different vendors.</p>
@@ -23,10 +24,10 @@ export default function Faq() {
             </AccordionCard>
             <AccordionCard title="Dark Mode and RTL Support">
               <p>Using modern CSS to handle a dark theme or support a different writing direction (RTL) is more manageable. If you use Spruce CSS the intended way, you will be okay with a new theme or RTL support.</p>
-              <p>Everything on this [documentation](/docs/getting-started/introduction/) page or under the [components](/ui/getting-started/introduction/) supports them with just a little code.</p>
+              <p>Everything on this <Link to="/docs/getting-started/introduction/">documentation</Link> page or under the <Link to="/ui/getting-started/introduction/">components</Link> supports them with just a little code.</p>
             </AccordionCard>
             <AccordionCard title="It Is Built on Sass">
-              <p>Sass can help a lot when writing a CSS framework or using one. You can use Spruce as a standalone CSS file, too, but to maximalize its potential, we suggest Sass. [For a simple compile setup](http://localhost:8000/blog/the-simplest-sass-compile-setup/), check out our related blog post.</p>
+              <p>Sass can help a lot when writing a CSS framework or using one. You can use Spruce as a standalone CSS file, too, but to maximalize its potential, we suggest Sass. <Link to="/blog/the-simplest-sass-compile-setup/">For a simple compile setup</Link>, check out our related blog post.</p>
             </AccordionCard>
           </AccordionList>
         </div>
