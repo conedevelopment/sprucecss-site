55709e13a363272a8ff5fd22f1c89860a6b30c3b
Adam Laki <info@adamlaki.com>
2023-04-21 09:50:45 +0200

    Update FAQ

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -14,14 +14,19 @@ export default function Faq() {
             description="Spruce CSS in nutshell."
           />
           <AccordionList>
-            <AccordionCard title="Why Make Another CSS Framework?">
-              <p>As you may know, there are many CSS frameworks (hundreds of them, and a lot of them are not maintained today). Everybody can choose one that suits their work style or project requirements. So why make another one? It is certainly not because we can do it better but because we want to do it our way. We want to be in control and make decisions.</p>
+            <AccordionCard title="Who is Spruce For?">
+              <p>If you like to handle your CSS/SCSS in separate files and write your code, this tool is for you. It uses just a few helper classes; most [components](/ui/getting-started/introduction/) are drop-ins.</p>
             </AccordionCard>
-            <AccordionCard title="It Is Opinionated">
-              <p>Each system is opinionated but on a different level; this is valid for Spruce too. We don’t want to vote for (strictly) any particular solution (because there is always more than one), but we will show you what we think is the best for us (and maybe for you too). We don’t believe there is a good or bad solution, but we can learn from any of them.</p>
+            <AccordionCard title="It is Modern and Lightweight">
+              <p>By modern, we mean it uses new CSS stuff sometimes at the expense of backward compatibility. Fortunately, nowadays, the new CSS features are released in bulk across different vendors.</p>
+              <p>With modern CSS, you can solve more with less code. It is around 1000 lines and wights a little. Also, you can choose what things you want to use to make it even smaller.</p>
             </AccordionCard>
-            <AccordionCard title="We Left the Grid Out">
-              <p>One controversial decision we made with Spruce is to leave a classical grid system out. Because of the late CSS layout model developments like Flexbox and Grid, we think it can be eliminated; this doesn’t mean that we won’t show you how to make layouts with ease, but we try to make it the modern way.</p>
+            <AccordionCard title="Dark Mode and RTL Support">
+              <p>Using modern CSS to handle a dark theme or support a different writing direction (RTL) is more manageable. If you use Spruce CSS the intended way, you will be okay with a new theme or RTL support.</p>
+              <p>Everything on this [documentation](/docs/getting-started/introduction/) page or under the [components](/ui/getting-started/introduction/) supports them with just a little code.</p>
+            </AccordionCard>
+            <AccordionCard title="It Is Built on Sass">
+              <p>Sass can help a lot when writing a CSS framework or using one. You can use Spruce as a standalone CSS file, too, but to maximalize its potential, we suggest Sass. [For a simple compile setup](http://localhost:8000/blog/the-simplest-sass-compile-setup/), check out our related blog post.</p>
             </AccordionCard>
           </AccordionList>
         </div>
